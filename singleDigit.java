5c79c732cbc6087b38ef5a22659d225f37972cf1
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-09-19 22:57:57 +0530

    last commit

--- singleDigit.java
+++ singleDigit.java
@@ -3,14 +3,21 @@ public class singleDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while(n > 9){
-            if(n % 2 != 0){
-                n = n/2;
-            } else{
-                n = (n-2)/2;
+        if(n >= -9 && n <= 9){
+            System.out.println(n);
+        } else{
+            int sign = (n < 0) ? -1 : 1;
+            n = Math.abs(n);
+
+            while( n > 9){
+                if(n % 2 != 0){
+                    n = n/2;
+                } else {
+                    n = (n-2)/2;
+                }
             }
+            System.out.println(sign * n);
         }
-        System.out.println(n);
         sc.close();
     }
 }
