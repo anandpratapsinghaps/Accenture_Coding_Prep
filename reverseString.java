4f4b01da82a7a09a35493111cc2743c08e4d7d14
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-10-03 10:26:26 +0530

    last commit

--- reverseString.java
+++ reverseString.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class reverseString {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of String : ");
         int n = sc.nextInt();
         sc.nextLine();
+        System.out.print("Enter your string : ");
         String s = sc.nextLine();
         System.out.println(new StringBuilder(s).reverse().toString());
     }
